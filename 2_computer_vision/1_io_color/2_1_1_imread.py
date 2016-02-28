9553a43ee35b8a693a7ae5d75a4542a54f9139a2
daiki_shimada_home <sheema.sheemap@gmail.com>
2016-02-28 15:10:10 +0900

    2-1-1 解答例ソースコードにコメントを追加

--- 2_computer_vision/1_io_color/2_1_1_imread.py
+++ 2_computer_vision/1_io_color/2_1_1_imread.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# 問題2-1-1 画像の読み込み
+# imageディレクトリにあるsample.tifを読み込み，その画像サイズとカラーチャンネル数を取得せよ．
+# Pillowでの解答例
+# Author: Daiki SHIMADA a.k.a kantoku
+
 from PIL import Image
 
 # 'r'ead mode
