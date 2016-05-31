3f9e8940dff4e8dade2f8bf1be398f5bb9512b66
maria97 <ysk2wsk.kahlua@gmail.com>
2016-05-31 19:45:54 +0900

    解答例001~003.py追加

---
+++ 1_warming-up/1_tbd1/002.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+
+"""
+02:文字数カウント
+    どっち使っても良いです.
+"""
+
+string = "hello python"
+
+print string,len(string)
+print string + " :{}".format( len(string) )
